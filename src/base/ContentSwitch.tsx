6c0a867bc6cc67fb823730cb0a8750f7dff4f07d
Timothy Emmanuel Kasasa <timothyk@kaboremus.no>
2019-01-25 20:58:55 +0300

    add proper home

--- src/base/ContentSwitch.tsx
+++ src/base/ContentSwitch.tsx
@@ -1,15 +1,12 @@
 import * as React from "react"
 import {Route, Switch} from 'react-router-dom'
 import {localRoutes} from "../data/constants";
-import Home from "../modules/Home";
+import Home from "../modules/Home2";
 import NoMatch from "./NoMatch";
 import Contacts from "../modules/contacts/Contacts";
 import Details from "../modules/contacts/Details";
 import Events from "../modules/events/Events";
 import Teams from "../modules/teams/Teams";
-// import NoMatch from "../modules/NoMatch";
-// import Home from "../modules/Home";
-// import {localRoutes} from "../data/constants";
 // import Users from "../modules/users/Users";
 import EventDetails from "../modules/events/details/EventDetails";
 import TeamDetails from "../modules/teams/Details";
@@ -17,10 +14,9 @@ import TeamDetails from "../modules/teams/Details";
 
 export class ContentSwitch extends React.Component {
     public render() {
-        console.log("Rendering>>>>>>>")
         return (
             <Switch>
-                <Route exact={true} path="/" component={Home}/>
+                <Route exact={true} path="/" component={Contacts}/>
                 <Route path={localRoutes.home} component={Home}/>
                 <Route path={localRoutes.contactsDetails} component={Details}/>
                 <Route path={localRoutes.contacts} component={Contacts}/>
@@ -30,7 +26,6 @@ export class ContentSwitch extends React.Component {
 
                 <Route path={localRoutes.teamsDetails} component={TeamDetails}/>
                 <Route path={localRoutes.teams} component={Teams}/>
-
                 {/*<Route path={localRoutes.users} component={Users}/>*/}
                 <Route component={NoMatch}/>
             </Switch>
